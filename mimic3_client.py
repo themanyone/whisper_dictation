fa315f0de2f413f389cfefa86b02d813f6f8fcb8
themanyone <nospam@thenerdshow.com>
2023-05-18 02:09:38 -0800

    use requests

--- mimic3_client.py
+++ mimic3_client.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3.10
-import os, sys
-from urllib.parse import urlencode
-# example url parsing
-# params = {
-    # 'name': 'John Doe',
-    # 'age': 30,
-    # 'city': 'New York'
-# }
-# query_string = urlencode(params)
+import sys
+import requests
+import subprocess
+import io
 
 def say(text):
-    params = { 'text': text }
-    query_string = "curl -s -X GET http://localhost:59125/api/tts?"
-    query_string += urlencode(params) + "| aplay -q -"
-    os.system(query_string)
+    base_url = "http://localhost:59125/api/tts"
+    params = { 'text': text, "lengthScale": "0.6" }
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        audio_data = io.BytesIO(response.content)
+        audio_bytes = audio_data.read()
+        # Pass the audio data to gstreamer-1.0 for playback
+        command = ['gst-launch-1.0', '-q', 'fdsrc', '!', 'wavparse', '!', 'autoaudiosink']
+        subprocess.run(command, input=audio_bytes, check=True,
+        stdout=subprocess.DEVNULL)
+    else:
+        print("Error:", response.status_code)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
